bb8db16103d7989df7c591bd59e4f32d2c0fb0d5
AreebAi <areebcalm@gmail.com>
2024-01-05 18:51:27 +0530

    schema for product and order

---
+++ models/productModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+
+const productSchema = new mongoose.Schema({
+    productName: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    qty:{Number,
+    required:true
+    },
+    imageURL: {
+        type: String, // Assuming the image URL will be a string
+        required: true,
+      },
+  });
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product
