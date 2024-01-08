3588147c578dca2fe0681384ccd5941002cf359f
AreebAi <areebcalm@gmail.com>
2024-01-08 19:52:23 +0530

    cors handling

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ import orderRoute from "./routers/orderRouter.js";
 import adminRoutes from "./routers/adminRoutes.js"
 dotenv.config();
 const app=express()
+const corsOptions = {
+    origin: 'http://localhost:3000', // Replace with the origin(s) you want to allow
+    optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+  };
+  
+  // Enable CORS for all routes
+  app.use(cors(corsOptions));
 app.use(cors())
 let dburl=process.env.DBURL
 let dbname=process.env.DBNAME
