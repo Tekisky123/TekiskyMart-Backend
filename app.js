970d234b2cd6f523cfa80b8fcc9830e12b7674af
AreebAi <areebcalm@gmail.com>
2024-01-08 18:41:54 +0530

    cors implementation

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
+import cors from "cors"
 import { dbConnect } from "./db/dbConnect.js"
 import orderRoute from "./routers/orderRouter.js";
 import adminRoutes from "./routers/adminRoutes.js"
 dotenv.config();
 const app=express()
+app.use(cors())
 let dburl=process.env.DBURL
 let dbname=process.env.DBNAME
 const port =process.env.PORT
