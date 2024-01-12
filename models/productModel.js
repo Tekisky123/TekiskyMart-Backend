eee0e6f7efd0a2cd832763570c425b3953a3cad8
muzammil shaikh <muzammilshaikh4373@gmail.com>
2024-01-12 15:37:23 +0500

    produact schema changes

--- models/productModel.js
+++ models/productModel.js
@@ -1,22 +1,26 @@
 import mongoose from "mongoose";
 
+const productDetailsSchema = new mongoose.Schema({
+  availablePackQty: { type: Number },
+  availableStockQty: { type: Number, required: true },
+  mrp: { type: Number, required: true },
+  offerPrice: { type: String, required: true, trim: true },
+  packetweight: { type: String, required: true, trim: true },
+});
+
 const productSchema = new mongoose.Schema(
   {
-    productId: { type: String, required: true, trim: true }, // Auto-generate elsewhere
-    productCategory: { type: String, required: true, trim: true }, // Added trim
-    productName: { type: String, required: true, trim: true }, // Added trim
-    productType: { type: String, required: true, trim: true }, // Added trim
-    productBrand: { type: String, required: true, trim: true }, // Added trim
-    description: { type: String, required: true, trim: true }, // Added trim
-    availablePackQty: { type: Number },
-    availableStockQty: { type: Number, required: true },
-    mrp: { type: Number, required: true },
-    offerPrice: { type: String, required: true, trim: true },
-    packetweight: { type: String, required: true, trim: true }, // Added trim
-    createdby: { type: String, required: true, trim: true }, // Added trim
+    productId: { type: String, required: true, unique: true, trim: true },
+    productCategory: { type: String, required: true, trim: true },
+    productName: { type: String, required: true, trim: true },
+    productType: { type: String, required: true, trim: true },
+    productBrand: { type: String, required: true, trim: true },
+    productDetails: [productDetailsSchema],
+    description: { type: String, required: true, trim: true },
+    createdby: { type: String, required: true, trim: true },
     imageURL: { type: Array, required: true },
     manufactureDate: { type: String, required: true, trim: true },
-    expiryDate: { type: String, required: true, trim: true }
+    expiryDate: { type: String, required: true, trim: true },
   },
   { timestamps: true }
 );
