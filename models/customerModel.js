680417d48355cad707fb5eb04d7bed047039deb2
Tekisky123 <153596067+Tekisky123@users.noreply.github.com>
2024-02-26 15:32:23 +0530

    Update customerModel.js
    Update customerModel.js

--- models/customerModel.js
+++ models/customerModel.js
@@ -4,7 +4,7 @@ const customerScehma = mongoose.Schema({
   customerName: { type: String, trim: true },
   mobileNumber: { type: String, trim: true, required: true },
   category: { type: String, trim: true },
-  knownFiled: { type: String, trim: true },
+  knownField: { type: String, trim: true },
 });
 
 const CustomerModel = mongoose.model("customerNumbers", customerScehma);
